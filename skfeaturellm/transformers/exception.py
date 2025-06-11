d03f2b794faa58113f6ffa96696ba1cc02d984a6
Roberto Corti <robi290596@gmail.com>
2025-06-11 21:53:50 +0200

    Add transformation validation

---
+++ skfeaturellm/transformers/exception.py
@@ -0,0 +1,2 @@
+class TransformerNotFoundError(Exception):
+    pass
--- skfeaturellm/transformers/schemas.py
+++ skfeaturellm/transformers/schemas.py
@@ -1,9 +1,35 @@
+from enum import Enum
 from typing import List, Literal, Optional, Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, field_validator
+
+import skfeaturellm.transformers as transformers
+from skfeaturellm.transformers.exception import TransformerNotFoundError
+
+
+class FeatureTransformationParameters(BaseModel):
+    pass
+
+
+class AdditiveTransformerParameters(FeatureTransformationParameters):
+    addend_cols: List[str]
+    subtract_cols: List[str]
+    skip_na: bool = False
 
 
 class FeatureTransformation(BaseModel):
     feature_name: str
     transformation: str
-    parameters: dict
+    parameters: FeatureTransformationParameters
+
+    @field_validator("transformation")
+    def transformation_must_exist(cls, v):
+        available = {
+            name.replace("Transformer", ""): getattr(transformers, name)
+            for name in getattr(transformers, "__all__", [])
+        }
+        if v not in available:
+            raise TransformerNotFoundError(
+                f"Transformation '{v}' not implemented. Available: {list(available.keys())}"
+            )
+        return v

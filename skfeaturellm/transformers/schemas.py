0d41292e4ead52d9f7b680f81d163d88202415bc
Roberto Corti <robi290596@gmail.com>
2025-06-11 22:04:00 +0200

    Simplify schemas in transformers

--- skfeaturellm/transformers/schemas.py
+++ skfeaturellm/transformers/schemas.py
@@ -7,29 +7,7 @@ import skfeaturellm.transformers as transformers
 from skfeaturellm.transformers.exception import TransformerNotFoundError
 
 
-class FeatureTransformationParameters(BaseModel):
-    pass
-
-
-class AdditiveTransformerParameters(FeatureTransformationParameters):
-    addend_cols: List[str]
-    subtract_cols: List[str]
-    skip_na: bool = False
-
-
 class FeatureTransformation(BaseModel):
     feature_name: str
     transformation: str
-    parameters: FeatureTransformationParameters
-
-    @field_validator("transformation")
-    def transformation_must_exist(cls, v):
-        available = {
-            name.replace("Transformer", ""): getattr(transformers, name)
-            for name in getattr(transformers, "__all__", [])
-        }
-        if v not in available:
-            raise TransformerNotFoundError(
-                f"Transformation '{v}' not implemented. Available: {list(available.keys())}"
-            )
-        return v
+    parameters: dict
